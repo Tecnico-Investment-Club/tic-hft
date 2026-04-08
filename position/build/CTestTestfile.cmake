2f8162813226bc5ad5743e8ff1dd7bdd3aaaef8a
Pedro Maltez <pedromaltez16.1@gmail.com>
2026-04-08 18:41:01 +0100

    Remove build folder from repo

--- position/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/pedro/Documents/GITHUB/Tecnico_Investment_Club/tic-hft/position
-# Build directory: /mnt/c/Users/pedro/Documents/GITHUB/Tecnico_Investment_Club/tic-hft/position/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(position_test "/mnt/c/Users/pedro/Documents/GITHUB/Tecnico_Investment_Club/tic-hft/position/build/position_test")
-set_tests_properties(position_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/c/Users/pedro/Documents/GITHUB/Tecnico_Investment_Club/tic-hft/position/CMakeLists.txt;23;add_test;/mnt/c/Users/pedro/Documents/GITHUB/Tecnico_Investment_Club/tic-hft/position/CMakeLists.txt;0;")
